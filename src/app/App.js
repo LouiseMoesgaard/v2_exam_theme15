82a52302250895380705e4c567581169e87aab1f
Sascha <sasc0520@stud.kea.dk>
2020-12-06 16:58:05 +0100

    cleaned up mac files

--- src/app/App.js
+++ src/app/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 import Dashboard from './pages/dashboard/Dashboard';
 import OrderForm from './pages/orderform/Orderform';
 import {Route, Switch} from 'react-router';
 import {BrowserRouter as Router} from 'react-router-dom';
+// import React, {useState, useEffect} from "react";
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {page: 1};
-    
+    this.state = {page: 1}; 
   }
-  
+  interval = null;
+    componentDidMount () {
+        this.interval = setInterval(() => {fetch("https://examspacebar.herokuapp.com/").then(r=>r.json()).then(data=>console.log(data))}, 10000);
+    }
+    componentWillUnmount () {
+        clearInterval(this.interval);
+    }
   setPage(number){
     this.page = number;
 
