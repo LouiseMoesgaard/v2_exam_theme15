213c62570ef85e2c5f1e7849086058b1d369db34
Sascha <sasc0520@stud.kea.dk>
2020-12-08 23:15:19 +0100

    sidenav made but needs hovereffect and more

--- src/app/pages/dashboard/Dashboard.js
+++ src/app/pages/dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavigationDash from '../../components/navigationDash/NavigationDash';
+import SideNavDash from '../../components/sideNavDash/SideNavDash';
 
 class Dashboard extends React.Component {
     render(){
         return (
             <div className="Dashboard">
             <NavigationDash />
+            <SideNavDash />
             </div>
         );
         
