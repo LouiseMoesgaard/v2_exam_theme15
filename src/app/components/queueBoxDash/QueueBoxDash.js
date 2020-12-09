f47024788d546572c089eaad52eeb341ea1fb31c
Sascha <sasc0520@stud.kea.dk>
2020-12-09 22:44:08 +0100

    all 3 overview boxes styled and responsive

--- src/app/components/queueBoxDash/QueueBoxDash.js
+++ src/app/components/queueBoxDash/QueueBoxDash.js
@@ -5,11 +5,29 @@ import './QueueBoxDash.scss';
 class QueueBoxDash extends React.Component {
     render() {
         return (
-        <div className="QueueBoxDash">
-         <div className="orderContainer">
-            <p>Placeholder text</p>
-         </div>
-        </div>
+        <div className="overviewDash">      
+            <div className="QueueBoxDash">
+                <h1>Orders...</h1>
+                <div className="orderContainer">
+                    <p>Placeholder text</p>
+                <p>Placeholder text</p>
+             </div>
+             </div>
+             <div className="ProcessBoxDash">
+                <h1>Processing...</h1>
+                <div className="orderContainer">
+                    <p>Placeholder text</p>
+                <p>Placeholder text</p>
+             </div>
+             </div>
+             <div className="ReadyBoxDash">
+                <h1>Ready...</h1>
+                <div className="orderContainer">
+                    <p>Placeholder text</p>
+                <p>Placeholder text</p>
+             </div>
+             </div>
+            </div>
        );
     }
 }
