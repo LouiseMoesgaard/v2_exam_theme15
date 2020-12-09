136041a092156f135c73fdcebc0ede424827e6d3
Sascha <sasc0520@stud.kea.dk>
2020-12-09 11:52:26 +0100

    queue box for overview initiated but not finished

---
+++ src/app/components/queueBoxDash/QueueBoxDash.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './QueueBoxDash.scss';
+
+class QueueBoxDash extends React.Component {
+    render() {
+        return (
+        <div className="QueueBoxDash">
+         <div className="orderContainer">
+            <p>Placeholder text</p>
+         </div>
+        </div>
+       );
+    }
+}
+
+export default QueueBoxDash;    
--- src/app/pages/dashboard/Dashboard.js
+++ src/app/pages/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavigationDash from '../../components/navigationDash/NavigationDash';
+import QueueBoxDash from '../../components/queueBoxDash/QueueBoxDash';
 import SideNavDash from '../../components/sideNavDash/SideNavDash';
 
 class Dashboard extends React.Component {
@@ -8,6 +9,7 @@ class Dashboard extends React.Component {
             <div className="Dashboard">
             <NavigationDash />
             <SideNavDash />
+            <QueueBoxDash />
             </div>
         );
         
