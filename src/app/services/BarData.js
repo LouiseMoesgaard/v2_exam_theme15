668a1957ba8606d7265b08d5f43cb9cdacba2f5f
Sascha <sasc0520@stud.kea.dk>
2020-12-13 01:28:57 +0100

    descriptions for all beers and are now shown in console

--- src/app/services/BarData.js
+++ src/app/services/BarData.js
@@ -1,3 +1,5 @@
+import BEERS from '../constants/Beers';
+
 const BarData = {
 
     getData: ()=> {
@@ -5,7 +7,7 @@ const BarData = {
         .then(r=>r.json())
         .then(data => {
             data.storage = data.storage.map(item => {
-                return {...item,price: 30}
+                return {...item,price: 30,desc: BEERS[item.name]}
             })
             return data;
         });
