caaab4859f1339a3b262bea84f7b064b9cf3f4e8
Sascha <sasc0520@stud.kea.dk>
2020-12-15 12:34:33 +0100

    3 columns for storage set up, might need more styling

--- src/app/components/statsDash/StatsDash.js
+++ src/app/components/statsDash/StatsDash.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BeerStorage from '../../components/beerStorage/BeerStorage';
 
 import './StatsDash.scss';
 
@@ -16,8 +17,18 @@ class statsDash extends React.Component {
              <div className="BeerStorage">
                 <h1>In stock...</h1>
                 <div className="storageContainer">
-                    <p>Placeholder text</p>
-                <p>Placeholder text</p>
+                    <div className="beersColumn">
+                        <h2>Beers</h2>
+                        <BeerStorage />
+                    </div>
+                    <div className="cidersColumn">
+                        <h2>Ciders</h2>
+                        <p>Placeholder text</p>
+                    </div>
+                    <div className="snacksColumn">
+                        <h2>Snacks</h2>
+                        <p>Placeholder text</p>
+                    </div>
              </div>
              </div>
         </div>
